1590d1768de02992aad51f59c2173be811682390
aishwarya <yash63882@gmail.com>
2024-03-15 13:23:00 +0530

    setup initiated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'SolarFaultDetection',
+    version= '1.0.0',
+    author= 'My Research Room',
+    author_email= 'boktiar@ineuron.ai',
+    packages= find_packages(),
+    install_requires = []
+
+)
