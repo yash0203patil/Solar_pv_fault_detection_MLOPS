564243964b3465bf1b724b04f3f2f42dd8c7fbd7
Aishwary Patil <90149608+yash0203patil@users.noreply.github.com>
2024-03-28 14:44:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'SolarFaultDetection',
     version= '1.0.0',
     author= 'My Research Room',
-    author_email= 'boktiar@ineuron.ai',
+    author_email= 'myresearchroom.ai@gmail.com',
     packages= find_packages(),
     install_requires = []
 
